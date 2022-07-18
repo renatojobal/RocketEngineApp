c9c2e635276a90128bca4905aadbd319f1a96201
renatojobal <renatojobal@gmail.com>
2022-07-17 23:03:56 -0500

    :art: add search bar

--- app/src/main/java/com/renatojobal/rocketEngine/ui/home/HomeScreen.kt
+++ app/src/main/java/com/renatojobal/rocketEngine/ui/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.renatojobal.rocketEngine.ui
 
+import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
@@ -7,24 +8,33 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.renatojobal.rocketEngine.R
 import com.renatojobal.rocketEngine.SharedViewModel
 import com.renatojobal.rocketEngine.model.Category
+import com.renatojobal.rocketEngine.ui.theme.RocketEngineTheme
 import timber.log.Timber
 
 @Composable
@@ -60,43 +70,45 @@ fun HomeScreen(sharedViewModel: SharedViewModel, onOffsetClicked: () -> Unit) {
             modifier = Modifier.fillMaxSize()
         ) {
 
-            selectedCategory?.let { safeCategory ->
-                Text(text = safeCategory.title + " " + safeCategory.thumbnail,
-                style = MaterialTheme.typography.h5,
-                modifier = Modifier.padding(top=8.dp))
-            }
+//            selectedCategory?.let { safeCategory ->
+//                Text(text = safeCategory.title + " " + safeCategory.thumbnail,
+//                style = MaterialTheme.typography.h5,
+//                modifier = Modifier.padding(top=8.dp))
+//            }
+//
+//            CircularProgressBar(
+//                percentage = currentPercentage,
+//                suffix = " kg",
+//                insideText = selectedCategory?.categoryCarbonFootprintKg?.trimDecimals() ?: "0"
+//            )
 
-            CircularProgressBar(
-                percentage = currentPercentage,
-                suffix = " kg",
-                insideText = selectedCategory?.categoryCarbonFootprintKg?.trimDecimals() ?: "0"
+            ExpandableSearchView(
+                searchDisplay = "",
+                onSearchDisplayChanged = {},
+                expandedInitially = true,
+                onSearchDisplayClosed = {}
             )
-            Button(
-                modifier = Modifier
-                    .padding(4.dp),
-                onClick = { onOffsetClicked() }
-            ) {
-                Text(text = "COMPENSAR", style = MaterialTheme.typography.button)
-            }
-            ActivitiesListPresenter(
-                sharedViewModel,
-                selectedCategory = selectedCategory
-            ) { wantedCategory ->
-                Timber.i("New category selected")
-                selectedCategory = wantedCategory
-
-            }
+//            Button(
+//                modifier = Modifier
+//                    .padding(4.dp),
+//                onClick = { onOffsetClicked() }
+//            ) {
+//                Text(text = "COMPENSAR", style = MaterialTheme.typography.button)
+//            }
+//            ActivitiesListPresenter(
+//                sharedViewModel,
+//                selectedCategory = selectedCategory
+//            ) { wantedCategory ->
+//                Timber.i("New category selected")
+//                selectedCategory = wantedCategory
+//
+//            }
         }
 
     }
 
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun HomeScreenPreview() {
-//    HomeScreen {}
-//}
 
 @Composable
 fun CircularProgressBar(
@@ -241,11 +253,171 @@ fun ActivitiesListPresenter(
 
 }
 
-//@Preview(showBackground = true)
-//@Composable
-//fun ActivitiesListPreview() {
-//    val dummyActivity1 = Category(percentage = 0.36, title = "Transport", thumbnail = "\uD83C\uDFE1")
-//    val dummyActivity2 = Category(percentage = 0.36, title = "Transport", thumbnail = "\uD83D\uDE8B")
-//    ActivitiesListPresenter()
-//}
+
+@Composable
+fun ExpandableSearchView(
+    searchDisplay: String,
+    onSearchDisplayChanged: (String) -> Unit,
+    onSearchDisplayClosed: () -> Unit,
+    modifier: Modifier = Modifier,
+    expandedInitially: Boolean = false,
+    tint: Color = MaterialTheme.colors.onPrimary
+) {
+    val (expanded, onExpandedChanged) = remember {
+        mutableStateOf(expandedInitially)
+    }
+
+
+    Crossfade(targetState = expanded) { isSearchFieldVisible ->
+        when (isSearchFieldVisible) {
+            true -> ExpandedSearchView(
+                searchDisplay = searchDisplay,
+                onSearchDisplayChanged = onSearchDisplayChanged,
+                onSearchDisplayClosed = onSearchDisplayClosed,
+                onExpandedChanged = onExpandedChanged,
+                modifier = modifier,
+                tint = tint
+            )
+
+            false -> CollapsedSearchView(
+                onExpandedChanged = onExpandedChanged,
+                modifier = modifier,
+                tint = tint
+            )
+        }
+    }
+}
+
+@Composable
+fun SearchIcon(iconTint: Color) {
+    Icon(
+        painter = painterResource(id = R.drawable.ic_search),
+        contentDescription = "search icon",
+        tint = iconTint
+    )
+}
+
+@Composable
+fun CollapsedSearchView(
+    onExpandedChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    tint: Color = MaterialTheme.colors.onPrimary,
+) {
+
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 2.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "Search",
+            style = MaterialTheme.typography.h6,
+            modifier = Modifier
+                .padding(start = 16.dp)
+        )
+        IconButton(onClick = { onExpandedChanged(true) }) {
+            SearchIcon(iconTint = tint)
+        }
+    }
+}
+
+@Composable
+fun ExpandedSearchView(
+    searchDisplay: String,
+    onSearchDisplayChanged: (String) -> Unit,
+    onSearchDisplayClosed: () -> Unit,
+    onExpandedChanged: (Boolean) -> Unit,
+    modifier: Modifier = Modifier,
+    tint: Color = MaterialTheme.colors.onPrimary,
+) {
+    val focusManager = LocalFocusManager.current
+
+    val textFieldFocusRequester = remember { FocusRequester() }
+
+    SideEffect {
+        textFieldFocusRequester.requestFocus()
+    }
+
+    var textFieldValue by remember {
+        mutableStateOf(TextFieldValue(searchDisplay, TextRange(searchDisplay.length)))
+    }
+
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.Start,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        IconButton(onClick = {
+            onExpandedChanged(false)
+            onSearchDisplayClosed()
+        }) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_back),
+                contentDescription = "back icon",
+                tint = tint
+            )
+        }
+        TextField(
+            value = textFieldValue,
+            onValueChange = {
+                textFieldValue = it
+                onSearchDisplayChanged(it.text)
+            },
+            trailingIcon = {
+                SearchIcon(iconTint = tint)
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .focusRequester(textFieldFocusRequester),
+            label = {
+                Text(text = "Search", color = tint)
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = {
+                    focusManager.clearFocus()
+                }
+            )
+        )
+    }
+}
+
+@Preview
+@Composable
+fun CollapsedSearchViewPreview() {
+    RocketEngineTheme {
+        Surface(
+            color = MaterialTheme.colors.primary
+        ) {
+            ExpandableSearchView(
+                searchDisplay = "",
+                onSearchDisplayChanged = {},
+                onSearchDisplayClosed = {}
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun ExpandedSearchViewPreview() {
+    RocketEngineTheme {
+        Surface(
+            color = MaterialTheme.colors.primary
+        ) {
+            ExpandableSearchView(
+                searchDisplay = "",
+                onSearchDisplayChanged = {},
+                expandedInitially = true,
+                onSearchDisplayClosed = {}
+            )
+        }
+    }
+}
+
+
 
